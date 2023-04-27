fe4cceb86d51cfc63dc9e6b975e999b89d0212c3
Ottobong <clemency50@gmail.com>
2023-04-27 12:56:41 +0100

    Updated code

--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -1,20 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './signup.scss';
 import FormInput from '../../component/formInput/FormInput'
 
 const Signup = () => {
+  const[inputValues, setInputValues]=useState({
+    username:"",
+    email:"",
+    password:"",
+    confirmPassword:"",
+});
+
+  const inputs = [
+    {
+      id: 1,
+      name: "username",
+      type: "text",
+      placeholder: "Username"
+    },
+    {
+      id: 2,
+      name: "email",
+      type: "email",
+      placeholder: "Email"
+    },
+    {
+      id: 3,
+      name: "password",
+      type: "password",
+      placeholder: "Password"
+    },
+    {
+      id: 4,
+      name: "confirmPassword",
+      type: "password",
+      placeholder: "Confirm Password"
+    },
+  ];
+
+  const handleChange =(e)=> {
+    setInputValues({...inputValues, [e.target.name]: e.target.value})
+  }
+
   return (
-    <div className='signup'>
-      
-    <form action="">
-      <FormInput placeholder='Username'/>
-      <FormInput placeholder='Email address'/>
-      <FormInput placeholder='Password'/>
-      <FormInput placeholder='Confirm password'/>
-    </form>
+    <div className='signup'>    
+      <form>
+        {inputs.map((input) => (
+          <FormInput key={input.id} 
+          {...input} 
+          value={inputValues[input.name]} 
+          onChange = {handleChange}
+          />
+        ))}
+        <button type='submit'>Signup</button>
+      </form>
 
     </div>
-  )
+  ) 
 }
 
 export default Signup
