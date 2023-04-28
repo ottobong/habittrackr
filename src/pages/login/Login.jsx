cd24f752bf3848c89848f62df0005f75911e12ae
Ottobong <clemency50@gmail.com>
2023-04-28 01:43:44 +0100

    added new lines of code to login

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,23 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react'
 import './login.scss';
 import { Link } from 'react-router-dom';
-import { FacebookTwoTone, Google } from '@mui/icons-material';
+import { FacebookTwoTone, Google, Visibility, VisibilityOff } from '@mui/icons-material';
 
 const Login = () => {
+
+  const [inputs, setInputs]= useState({
+    email: "",
+    password: "",
+  });
+
+  const[toggleEye, setToggleEye] = useState(false);
+  const[inputType, setInputType] = useState("password")
+
+  const handleToggle = (e) => {
+    setToggleEye(!toggleEye);
+    setInputType(inputType === "password" ? "text" : "password")
+  };
+
+  const handleChange = (e)=>{
+    setInputs((prev) => ({ ...prev, [e.target.name]:e.target.value})
+    );
+  }
+
   return (
     <div className='login'>
       <form>
         <h2>Login</h2>
-
-        <button type='submit'>Signup</button>
+        <div className='formInput'>
+          <input 
+          type="email"
+          name="email" 
+          placeholder="Email"
+          onChange={handleChange}
+          required 
+          />
+        </div>
+        <div className='formInput'>
+          <input 
+          type={inputType}
+          name="password" 
+          id="password"
+          placeholder="Password"
+          onChange={handleChange}
+          required 
+          />
+          <div className="eyeIcon" onClick={handleToggle}>
+            {toggleEye ? <Visibility /> : <VisibilityOff />}
+          </div>
+        </div>
+        <button type='submit'>Login</button>
         <div className="formLink">
-          <span>Already have an account?</span>
+          <span>Don't have an account?</span>
           <Link 
-          to='/login' 
+          to='/signup' 
           className='formSignup'
           style={{textDecoration: 'none'}}>
             {" "}
-            Login
+            Signup now
             </Link>
         </div>
 
